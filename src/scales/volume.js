c7fba4755776d26c9080a711ff9edc640395eaf1
Susisu <susisu2413@yahoo.co.jp>
2016-05-05 11:25:01 +0900

    Add volume scales

--- src/scales/volume.js
+++ src/scales/volume.js
@@ -8,6 +8,41 @@
 import { SIUnit } from "../units.js";
 
 let values = [
+    {
+        value: 2.82e-45,
+        description:
+            `Proton`
+    },
+    {
+        value: 7.23e-30,
+        description:
+            `Hydrogen atom`
+    },
+    {
+        value: 5e-21,
+        description:
+            `A virus`
+    },
+    {
+        value: 9e-17,
+        description:
+            `Human red blood cell`
+    },
+    {
+        value: 2e-15,
+        description:
+            `Ink drop of a printer`
+    },
+    {
+        value: 6.2e-11,
+        description:
+            `A medium grain of sand`
+    },
+    {
+        value: 2e-8,
+        description:
+            `A grain of rice`
+    }
 ];
 
 export default values.map(x => ({
