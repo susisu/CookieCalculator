3d47fef444c452ebf41e6649d61d589208b4f700
Susisu <susisu2413@yahoo.co.jp>
2016-05-06 12:35:45 +0900

    Add volume scales

--- src/scales/volume.js
+++ src/scales/volume.js
@@ -42,6 +42,51 @@ let values = [
         value: 2e-8,
         description:
             `A grain of rice`
+    },
+    {
+        value: 2e-7,
+        description:
+            `Pea`
+    },
+    {
+        value: 5e-6,
+        description:
+            `One teaspoon`
+    },
+    {
+        value: 1.5e-5,
+        description:
+            `One tablespoon`
+    },
+    {
+        value: 7.5e-4,
+        description:
+            `Wine bottle`
+    },
+    {
+        value: 1.4e-3,
+        description:
+            `Human brain cavity`
+    },
+    {
+        value: 5e-3,
+        description:
+            `Human blood`
+    },
+    {
+        value: 6e-2,
+        description:
+            `Gasoline fuel tank in a car`
+    },
+    {
+        value: 7.1e-2,
+        description:
+            `Human`
+    },
+    {
+        value: 2e-1,
+        description:
+            `Drum`
     }
 ];
 
